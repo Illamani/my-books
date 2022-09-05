45fe72697107d3a470129945210bc9c0e3adf81f
WB Vera <WBVera@rosariobus.com.ar>
2022-09-05 14:18:28 -0300

    Primer HttpPost

---
+++ Data/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace my_books.Data.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
